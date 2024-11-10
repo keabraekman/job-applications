3eee129914914415e8d954133c678c033d2ed968
keabraekman <kea.braekman@mail.com>
2024-11-10 10:56:32 -0800

    added resume

--- search.py
+++ search.py
@@ -4,40 +4,26 @@ from dotenv import load_dotenv
 from job_scraper_utils import *
 import json
 from openai import OpenAI
+from datetime import datetime
 
 load_dotenv()
-
-
 api_key = os.getenv('OPENAI_API_KEY')
 united_states = 'https://www.indeed.com'
-
 country = united_states
 
 driver = configure_webdriver()
-job_position = 'devops engineer'
 # job_positions = ['devops engineer', 'security engineer', 'cloud engineer', 'backend', 'AWS', 'Terraform', 'Gitlab', 'Python', 'Docker', 'Bash', 'systems enginer', 'software engineer']
 job_positions = ['terraform']
 job_location = '90066'
 date_posted = 1
 pay = '$150,000'
-
 sorted_df = None
 
-from datetime import datetime
-
-# Get today's date in the desired format
-today_date = datetime.today().strftime('%Y-%m-%d')  # Format as 'YYYY-MM-DD'
-# Construct the filename
+today_date = datetime.today().strftime('%Y-%m-%d')
 filename = f"{today_date}.json"
-
-
 full_url = search_jobs(pay, driver, country, 'devops engineer', job_location, date_posted)
 df = scrape_job_data(driver, country)
-# driver.quit()
-
-
 dataframes = []
-
 for job_position in job_positions:
     full_url = search_jobs(pay, driver, country, job_position, job_location, date_posted)
     df = scrape_job_data(driver, country)
@@ -45,20 +31,15 @@ for job_position in job_positions:
 
 # Concatenate all dataframes and remove duplicates
 merged_df = pd.concat(dataframes).drop_duplicates().reset_index(drop=True)
-
 merged_df.to_json(filename, orient='records')
-
 json_data = merged_df.to_json(orient="records")
-
 # Parse and pretty-print JSON
 parsed_json = json.loads(json_data)
 with open(filename, "w") as f:
     json.dump(parsed_json, f, indent=4)
 
 
-
-# We want to create a function that adds the job description in the JSON output
-
+# This function takes a URL and returns the full description as a string.
 def jobDescription(url):
     driver.get(url)
     description_element = driver.find_element(By.XPATH, '//div[contains(@class, "jobsearch-JobComponent-description")]')
@@ -69,10 +50,6 @@ def jobDescription(url):
     else:
         description = description
     return description
-
-
-
-print(jobDescription('https://www.indeed.com/rc/clk?jk=56da6e5acbeb0d81&bb=qpWnEreCP8py8fL5SNfQwAbrTTkJU78Lb1aLYFitamL_QG1nwMmsm1WCHcVV7vK1EIAxF_ZiksxaAozW_5TUkIwjWhiIwM3YVZoC0o3xZOLgq3JPs8_oGSco3HLxCy2E&xkcb=SoDZ67M35KbfTTgJAL0JbzkdCdPP&fccid=a0ba1788fee5b9f4&vjs=3'))
 
 
 def generate_resume(job_description):
@@ -107,26 +84,39 @@ def generate_resume(job_description):
         max_tokens=1000,
         temperature=0.7,
     )
-
-    # Extracting the resume content from the response
     print('response = ', response.choices[0].message.content)
-    # for k in response:
-    #     print('k = ', k)
-    #     print(response[k])
-    # resume = response['choices'][0]['message']['content']
     return 'END'
 
-# Example job description
-job_description = """
-Principal Technical Consultant, Platform Engineering
-Serving as a technical thought leader and SME for our ecosystem of partners, customers, and service providers in the realm of Modern Apps & Platform Engineering, in addition to serving as a leader of other teammates. The Principal Technical Consultant works as part of the consulting team to lead the development, design, implementation, and engagement leader of client solutions. Principal Technical Consultants manage, mentor and delegate to engineers on a project basis, manage and maintain career advancement plans for the team, and within the broader organization as it relates to internal initiatives and indirect project support. This role requires a strategic mindset, strong leadership abilities, and deep technical proficiency across various domains.
-Responsibilities...
-"""
 
 # Generate the resume
-resume_text = generate_resume(job_description)
+description = jobDescription('https://www.indeed.com/viewjob?jk=61346edc64651631&from=serp&vjs=3')
+resume_text = generate_resume(description)
 print(resume_text)
 
-
-
 driver.quit()
+
+base_resume = """
+Booz Allen Hamilton Staff Engineer | Sept. 2022 – Present Aug. 2020 – Present
+Remote
+▪ Full Services Team - Hybrid Legacy Application Migration to AWS.
+o Increased scalability and reliability of 6+ applications by 40%, migrating
+hybrid infrastructure to AWS ECS, ECR, RDS, and MQ with Terraform.
+o Reduced end-to-end application deployment time and manual steps by 50% using
+Gitlab CI, Docker, and Terraform.
+o Integrated Artifactory with AWS ECR for container image storage, simplifying
+artifact management and versioning for 20+ Dockerized applications.
+▪ Security Services Team - Deployed and Maintained Security Infrastructure.
+o Deployed and maintained Tenable SC and Trend Micro across 10+ multi-region AWS
+accounts, significantly decreased vulnerabilities by automating security scans.
+o Improved the patching process for security assets which saved 5 hours a week by
+automating updates using AWS Inspector, Lambda, and Step Functions.
+Software Engineer | Dec. 2020 – Sept. 2022
+▪ Foundational Services Team
+o Automated TLS certificate creation by developing a Gitlab CI pipeline leveraging
+AWS ACM across 50+ instances.
+▪ Systems Engineer | Aug. 2020 - Dec. 2020
+o Developed a web scraping Python bot that saved 160+ hours of manual data entry
+by curling client financial data into an Excel spreadsheet.
+o Researched and drafted proposals for Cloud Engineering and FedRAMP projects for
+the Department of Defense.
+"""
